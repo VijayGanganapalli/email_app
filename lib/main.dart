f511cccdda8f204b13f849b10369dac4f92d2e2e
Vijay <vijayganganapalli@gmail.com>
2019-03-02 07:08:59 +0530

    home screen created

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 void main() => runApp(EmailApp());
 
 class EmailApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Email App',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: HomeScreen(title: 'Email App'),
+    );
+  }
+
+}
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
   @override
   Widget build(BuildContext context) {
     return Container();
