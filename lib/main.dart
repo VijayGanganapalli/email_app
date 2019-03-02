a0afb078be3e59464aa87549b1c4ae48a7e657c0
Vijay <vijayganganapalli@gmail.com>
2019-03-02 06:00:42 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(EmailApp());
+
+class EmailApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
