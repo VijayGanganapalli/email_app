25ddd4603be250a3e65dd39c959903e349ee9675
Vijay <vijayganganapalli@gmail.com>
2019-03-05 05:51:11 +0530

    added list of messages

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 void main() => runApp(EmailApp());
 
 class EmailApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,16 +13,46 @@ class EmailApp extends StatelessWidget {
       home: HomeScreen(title: 'Email App'),
     );
   }
-
 }
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({Key key, this.title}) : super(key: key);
 
   final String title;
+  final List<String> messages = const [
+    'My first message',
+    'You won lottery',
+    'You should read this message',
+    'An email from Google'
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+      ),
+      body: ListView.separated(
+        itemCount: messages.length,
+        separatorBuilder: (BuildContext context, int index) => Divider(),
+        itemBuilder: (BuildContext context, int index) {
+          var title = messages[index];
+          return ListTile(
+            leading: CircleAvatar(
+              child: Text('VK'),
+            ),
+            title: Text(title),
+            subtitle: Text(
+              'This message is very very long so you can easily understand to read and also see. You should reply after see it.',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              ),
+
+            isThreeLine: true,
+
+          );
+        },
+      ),
+    );
   }
 }
