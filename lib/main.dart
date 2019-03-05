8d46a5018ae8152a0d56a337a3b347a55b6e28f9
Vijay <vijayganganapalli@gmail.com>
2019-03-05 07:08:31 +0530

    refactoring homescreen as messagelist

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:email_app/message_list.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(EmailApp());
@@ -10,42 +11,7 @@ class EmailApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
-      home: HomeScreen(title: 'Email App'),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  HomeScreen({Key key, this.title}) : super(key: key);
-
-  final String title;
-  final messages = const [];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(this.title),
-      ),
-      body: ListView.separated(
-        itemCount: messages.length,
-        separatorBuilder: (BuildContext context, int index) => Divider(),
-        itemBuilder: (BuildContext context, int index) {
-          var message = messages[index];
-          return ListTile(
-            leading: CircleAvatar(
-              child: Text('VK'),
-            ),
-            title: Text(message['subject']),
-            subtitle: Text(
-              message['body'],
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            isThreeLine: true,
-          );
-        },
-      ),
+      home: MessageList(title: 'Email App'),
     );
   }
 }
