9e7193a0216aac43cf95bc7249f2d26139c46ef8
Vijay <vijayganganapalli@gmail.com>
2019-03-07 04:32:44 +0530

    async, await and futurebuilder

--- lib/message.dart
+++ lib/message.dart
@@ -1,4 +1,6 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 part 'message.g.dart';
 
@@ -8,5 +10,19 @@ class Message {
   final String body;
 
   Message(this.subject, this.body);
-  factory Message.fromJson(Map<String, dynamic> json) => _$MessageFromJson(json);
+  factory Message.fromJson(Map<String, dynamic> json) =>
+      _$MessageFromJson(json);
+
+  static Future<List<Message>> browse() async {
+    http.Response response =
+        await http.get('http://www.mocky.io/v2/5c803a5933000072628485c4');
+    await Future.delayed(Duration(seconds: 3));
+    String content = response.body;
+
+    List collection = json.decode(content);
+    List<Message> _messages =
+        collection.map((json) => Message.fromJson(json)).toList();
+
+    return _messages;
+  }
 }
--- lib/message_list.dart
+++ lib/message_list.dart
@@ -1,7 +1,5 @@
 import 'package:email_app/message.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'dart:convert';
 
 class MessageList extends StatefulWidget {
   MessageList({Key key, this.title}) : super(key: key);
@@ -13,49 +11,67 @@ class MessageList extends StatefulWidget {
 }
 
 class _MessageListState extends State<MessageList> {
-  var messages = const [];
-
-  Future loadMessageList() async {
-    String content = await rootBundle.loadString('data/message.json');
-    List collection = json.decode(content);
-    List<Message> _messages =collection.map((json) => Message.fromJson(json)).toList();
-    
-    setState(() {
-     messages = _messages; 
-    });
-  }
+  Future<List<Message>> messages;
 
   @override
   void initState() {
-    loadMessageList();
     super.initState();
+    messages = Message.browse();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(this.widget.title),
-      ),
-      body: ListView.separated(
-        itemCount: messages.length,
-        separatorBuilder: (BuildContext context, int index) => Divider(),
-        itemBuilder: (BuildContext context, int index) {
-          Message message = messages[index];
-          return ListTile(
-            leading: CircleAvatar(
-              child: Text('VK'),
-            ),
-            title: Text(message.subject),
-            subtitle: Text(
-              message.body,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            isThreeLine: true,
-          );
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: Text(this.widget.title),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {
+                var _messages = Message.browse();
+                setState(() {
+                  messages = _messages;
+                });
+              },
+            )
+          ],
+        ),
+        body: FutureBuilder(
+          future: messages,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+              case ConnectionState.active:
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              case ConnectionState.done:
+                if (snapshot.hasError)
+                  return Text('There was an error: ${snapshot.error}');
+                var messages = snapshot.data;
+                return ListView.separated(
+                  itemCount: messages.length,
+                  separatorBuilder: (BuildContext context, int index) =>
+                      Divider(),
+                  itemBuilder: (BuildContext context, int index) {
+                    Message message = messages[index];
+                    return ListTile(
+                      leading: CircleAvatar(
+                        child: Text('VK'),
+                      ),
+                      title: Text(message.subject),
+                      subtitle: Text(
+                        message.body,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      isThreeLine: true,
+                    );
+                  },
+                );
+            }
+          },
+        ));
   }
 }
