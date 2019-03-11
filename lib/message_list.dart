c7722262ea7f19128f1f128c3b4998ac12ea98f3
Vijay <vijayganganapalli@gmail.com>
2019-03-11 23:46:55 +0530

    added side menu with drawer widjet

--- lib/message_list.dart
+++ lib/message_list.dart
@@ -2,6 +2,7 @@ import 'package:email_app/message.dart';
 import 'package:email_app/compose_button.dart';
 import 'package:email_app/message_detail.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MessageList extends StatefulWidget {
   MessageList({Key key, this.title}) : super(key: key);
@@ -43,6 +44,73 @@ class _MessageListState extends State<MessageList> {
             },
           )
         ],
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://avatars2.githubusercontent.com/u/28445596?s=460&v=4'),
+              ),
+              accountName: Text('Vijay Ganganapalli'),
+              accountEmail: Text('vijayganganapalli@gmail.com'),
+              otherAccountsPictures: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text('Adding new account...'),
+                          );
+                        });
+                  },
+                  child: CircleAvatar(
+                    child: Text('+'),
+                  ),
+                ),
+              ],
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.inbox),
+              title: Text('Inbox'),
+              trailing: Chip(
+                label: Text(
+                  '11',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                backgroundColor: Colors.grey,
+              ),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.edit),
+              title: Text('Draft'),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.archive),
+              title: Text('Archive'),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.paperPlane),
+              title: Text('Sent'),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.trash),
+              title: Text('Trash'),
+            ),
+            Divider(),
+            Expanded(
+              child: Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: ListTile(
+                  leading: Icon(FontAwesomeIcons.cog),
+                  title: Text('Settings'),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
       body: FutureBuilder(
         future: future,
